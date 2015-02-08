8cb7dbe16db64a2b7e4a5b9bf0458241817f9cee
Anirudhan Jegannathan Rajagopalan <ajr619@energon1.cims.nyu.edu>
2015-02-08 11:45:46 -0500

    adding all required ds and readers

--- linker1/linker.cpp
+++ linker1/linker.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char* argv[]){
+  if(argc != 2){
+    cout << "Command should be called with the input file as argument.";
+  }
 	char ch;
   fstream fin("file", fstream::in);
   while(fin >> noskipws >> ch){

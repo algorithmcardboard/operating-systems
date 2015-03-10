e72f5a57c5e52db8a4a6eb1d9887b4526f035f55
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2015-03-10 10:01:31 -0400

    Wrapping up lcfs scheduler

--- lab2/src/scheduler/lcfs.cpp
+++ lab2/src/scheduler/lcfs.cpp
@@ -1,16 +1,31 @@
 #include "abstract_scheduler.h"
 
-class  LCFS : public Scheduler{
+#include <stack>
 
+class LCFS : public Scheduler{
+  private:
+    stack<Process*>* runQueue;
   public:
-    LCFS():Scheduler(){}
-    LCFS(int a):Scheduler(a){}
+    LCFS():Scheduler(){
+      runQueue = new stack<Process*>();
+    }
+    LCFS(int a):Scheduler(a){
+      runQueue = new stack<Process*>();
+    }
 
     void addProcess(Process* p){
-      cout << "In add process flow";
+      p->resetDynamicPriority();
+      runQueue->push(p);
     };
 
     Process* get_next_process(){
-      return NULL;
+      if(runQueue->size() <= 0){
+        return NULL;
+      }
+      Process* p = runQueue->top();
+      if(p != NULL){
+        runQueue->pop();
+      }
+      return p;
     }
 };

ddf3c672a9b9187c6d7b2ccaf521f2a10aa93149
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2015-04-22 07:20:45 -0400

    wrapping up aging

--- lab3/src/pr_algos/aging.cpp
+++ lab3/src/pr_algos/aging.cpp
@@ -8,10 +8,7 @@ class Aging:public AbstractPR{
   public:
     Aging(vector<pte>* pt, vector<unsigned int>* ft, vector<unsigned int>* ftop, int nf): AbstractPR(pt, ft, ftop){
       numFrames = nf;
-      counter = new vector<unsigned int>();
-      for(int i = 0; i < numFrames; i++){
-        counter->push_back(0);
-      }
+      counter = new vector<unsigned int>(nf, 0);
     }
 
     int get_frame_to_replace(){
@@ -21,21 +18,15 @@ class Aging:public AbstractPR{
         physical_frame = frame_table->at(i);
         pt_index = ftop->at(physical_frame);
 
-        unsigned int cValue = counter->at(i);
-        cValue >> 1;
+        unsigned int& cValue = counter->at(i);
 
-        if(page_table->at(pt_index).referenced == 1){
-          cValue = cValue | 0x80000000;
-          page_table->at(pt_index).referenced = 0;
-        }
+        cValue = (cValue >> 1) | (page_table->at(pt_index).referenced << 31);
+        page_table->at(pt_index).referenced = 0;
 
         counter->at(i) = cValue;
       }
 
-      //cout << "done with shifting " << endl;
-      //cout << frame_table->size() << " " << counter->size() << endl;
-
-      unsigned int min = INT_MAX, tmp, min_index, fNumber = 0;
+      unsigned int min = 0xffffffff, tmp, min_index, fNumber = 0;
       for(int i = 0; i < frame_table->size(); i++){
         physical_frame = frame_table->at(i);
         tmp = counter->at(i);

9450ae0bcb293886f797104533695c9a73a5ef6d
Anirudhan Jegannathan <anirudhan@rajegannathan.in>
2015-05-01 12:48:55 -0400

    skeleton schedulers

--- lab4/src/init.cpp
+++ lab4/src/init.cpp
@@ -5,6 +5,20 @@
 using namespace std;
 
 int main(int argc, char** argv){
+  bool verbose = false, fscan = false, request_details = false;
   while((c = getopt(argc, argv, "s:vfd:")) != -1){
+    switch(c){
+      case 's':
+        break;
+      case 'v':
+        verbose = true;
+        break;
+      case 'f':
+        fscan = true;
+        break;
+      case 'd':
+        request_details = true;
+        break;
+    }
   }
 }
